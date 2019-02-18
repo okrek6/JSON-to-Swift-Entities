cd7c62e772a26fc2e3f70756ddd63113386c792a
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2019-02-17 20:21:59 -0600

    Created Projects

---
+++ JSON-to-Swift-Entities/JSONSerialization/JSONSerialization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONSerialization
+//
+//  Created by Brendan Krekeler on 2/17/19.
+//  Copyright © 2019 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
